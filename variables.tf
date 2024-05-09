569e71b6845fe80dd5283ae94819d7606638a1f1
Phone Myint Myat Zaw <phonemyintmyatzaw7@gmail.com>
2024-05-09 16:17:04 +0630

    modified main.tf & outputs.tf

--- variables.tf
+++ variables.tf
@@ -2,32 +2,36 @@
 variable "client_id" {
   type        = string
   description = "define client id"
+  sensitive = true
 }
 
 variable "client_secret" {
   type        = string
   description = "define client secret"
+  sensitive = true
 }
 
 variable "subscription_id" {
   type        = string
   description = "define subscription id"
+  sensitive = true
 }
 
 variable "tenant_id" {
   type        = string
   description = "define tenant id"
+  sensitive = true
 }
 
 // Resource Group
 variable "resource_group_name" {
   type        = string
   description = "define resource group name"
-  default = "WebserverRG"
+  default     = "WebserverRG"
 }
 
 variable "location" {
-    type = string
-    description = "define location of resources"
-    default = "Southeast Asia"
+  type        = string
+  description = "define location of resources"
+  default     = "Southeast Asia"
 }
