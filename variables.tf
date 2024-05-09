ed171f1ce51e154d068e1edb052f85d15facbac0
Phone Myint Myat Zaw <phonemyintmyatzaw7@gmail.com>
2024-05-09 15:54:00 +0630

    modified gitignore file

--- variables.tf
+++ variables.tf
@@ -1,20 +1,33 @@
 // Authentication to Azure 
 variable "client_id" {
-    type = string
-    description = "define client id"
+  type        = string
+  description = "define client id"
 }
 
 variable "client_secret" {
-    type = string
-    description = "define client secret"
+  type        = string
+  description = "define client secret"
 }
 
 variable "subscription_id" {
-    type = string
-    description = "define subscription id"
+  type        = string
+  description = "define subscription id"
 }
 
 variable "tenant_id" {
+  type        = string
+  description = "define tenant id"
+}
+
+// Resource Group
+variable "resource_group_name" {
+  type        = string
+  description = "define resource group name"
+  default = "WebserverRG"
+}
+
+variable "location" {
     type = string
-    description = "define tenant id"
+    description = "define location of resources"
+    default = "Southeast Asia"
 }
