adaafa4c0fd4a78c6bf6154d154e733c6837190e
Phone Myint Myat Zaw <phonemyintmyatzaw7@gmail.com>
2024-05-09 15:41:39 +0630

    added variable and service principal

---
+++ variables.tf
@@ -0,0 +1,20 @@
+// Authentication to Azure 
+variable "client_id" {
+    type = string
+    description = "define client id"
+}
+
+variable "client_secret" {
+    type = string
+    description = "define client secret"
+}
+
+variable "subscription_id" {
+    type = string
+    description = "define subscription id"
+}
+
+variable "tenant_id" {
+    type = string
+    description = "define tenant id"
+}
